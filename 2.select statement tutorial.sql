a22b0de862cacae5af7fd0a50490df058b5bc636
sidraaaaaaaaa <163036788+sidraaaaaaaaa@users.noreply.github.com>
2024-04-09 10:41:17 -0400

    Add files via upload
    PracticeSqlQuries

---
+++ 2.select statement tutorial.sql
@@ -0,0 +1,13 @@
+SELECT *
+from parks_and_recreation.employee_demographics;
+
+SELECT first_name,
+last_name,
+birth_date,
+age,
+age + 10
+from parks_and_recreation.employee_demographics;
+# PEMDAS Order of calculation
+
+ select distinct first_name, gender
+ from  parks_and_recreation.employee_demographics;
